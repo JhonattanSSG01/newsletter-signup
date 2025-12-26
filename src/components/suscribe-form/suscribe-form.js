087fb43734a5e33a63c74eb80f40a18ba5c7e75f
stiven_saavedra <stiven_saavedra@epamneoris.com>
2025-12-26 12:09:00 -0500

    Layout of the subscription form

--- src/components/suscribe-form/suscribe-form.js
+++ src/components/suscribe-form/suscribe-form.js
@@ -1,13 +1,32 @@
 import { LitElement } from 'lit';
 import { customElements } from "lit/decorators.js";
 
-@customElements("suscribe-form")
 export class SuscribeForm extends LitElement {
   render() {
     return html`
-      <div>
-        <slot></slot>
-      </div>
+        <div>
+          <article>
+            <h1>Stay updated!</h1>
+            <p>
+              Join 60,000+ product managers receiving monthly updates on:</p>
+            <p>
+              <ul>
+                <li>Product discovery and building what matters</li>
+                <li>Measuring to ensure updates are a success</li>
+                <li>And much more!</li>
+              </ul>
+            </p>
+            <section>
+              <input type="email" placeholder="Email address">
+              <button>Subscribe to monthly newsletter</button>
+            </section>
+          </article>
+          <section>
+            <img src="./src/assets/img/banner-mobile.jpg" alt="illustration-sign-up-mobile">
+          </section>
+        </div>
     `;
   }
 }
+
+customElements.define('suscribe-form', SuscribeForm);
--- src/sign-up.js
+++ src/sign-up.js
@@ -1,5 +1,6 @@
 import { LitElement, css, html } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
+import "./components/suscribe-form/suscribe-form.js"
 
 @customElement('sign-up')
 export class SignUp extends LitElement {
@@ -10,8 +11,8 @@ export class SignUp extends LitElement {
 
   render() {
     return html`
-      <div>
-      </div>
+      <suscribe-form">
+      </suscribe-form">
 
     `
   }
@@ -29,68 +30,6 @@ export class SignUp extends LitElement {
         text-align: center;
       }
 
-      .logo {
-        height: 6em;
-        padding: 1.5em;
-        will-change: filter;
-        transition: filter 300ms;
-      }
-      .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-      }
-      .logo.lit:hover {
-        filter: drop-shadow(0 0 2em #325cffaa);
-      }
-
-      .card {
-        padding: 2em;
-      }
-
-      .read-the-docs {
-        color: #888;
-      }
-
-      a {
-        font-weight: 500;
-        color: #646cff;
-        text-decoration: inherit;
-      }
-      a:hover {
-        color: #535bf2;
-      }
-
-      ::slotted(h1) {
-        font-size: 3.2em;
-        line-height: 1.1;
-      }
-
-      button {
-        border-radius: 8px;
-        border: 1px solid transparent;
-        padding: 0.6em 1.2em;
-        font-size: 1em;
-        font-weight: 500;
-        font-family: inherit;
-        background-color: #1a1a1a;
-        cursor: pointer;
-        transition: border-color 0.25s;
-      }
-      button:hover {
-        border-color: #646cff;
-      }
-      button:focus,
-      button:focus-visible {
-        outline: 4px auto -webkit-focus-ring-color;
-      }
-
-      @media (prefers-color-scheme: light) {
-        a:hover {
-          color: #747bff;
-        }
-        button {
-          background-color: #f9f9f9;
-        }
-      }
     `
   }
 }
